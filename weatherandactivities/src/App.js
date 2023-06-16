3113e674c78c2e32f196b1ff0b71b3f4681c00f0
Nils Otto <nilsotto@gmx.de>
2023-06-16 11:49:43 +0200

    add form with add functionality

--- weatherandactivities/src/App.js
+++ weatherandactivities/src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
+import Form from "./components/form/Form.js";
+import React, { useState } from "react";
+import { uid } from "uid";
 
-function App() {
+export default function App() {
+  const [activities, setActivities] = useState([]);
+
+  function handleAddActivity(newActivity) {
+    setActivities([...activities, { id: uid(), ...newActivity }]);
+  }
+  console.log(activities);
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Form onAddActivity={handleAddActivity} />
     </div>
   );
 }
-
-export default App;
